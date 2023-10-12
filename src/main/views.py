265c8cd09d3c7987f49fb904cfc8155b412d3838
Emmie Abels <emmieabels1@gmail.com>
2023-10-12 13:19:11 -0500

    Added form frontend componenets

--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .forms.login_form import LoginForm
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
+# Temporary Signup view. Will be updated later
+def signup(request):
+    return render(request, "signup.html")
+
 def login(request):
-    return render(request, 'login.html')
+    # if request.method == 'POST': # django login forms use POST method
+    #     form = LoginForm(request.POST) # send form to server
+    # add code to check if information sent to server is valid
+
+    return render(request, "login.html")
