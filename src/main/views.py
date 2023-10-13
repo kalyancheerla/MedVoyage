7d035f91d9ea8e4af8618cf7a36ac02094d0d9ea
Bhavani Rachakatla <gangaputrikarb20@gmail.com>
2023-10-12 21:13:30 -0500

    Initial commit for About Us and Contact Us pages

--- src/main/views.py
+++ src/main/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def about_us(request):
+    return render(request, "about_us.html")
+
+def contact_us(request):
+    return render(request,"contact_us.html")
