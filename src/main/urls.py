4bb7f76a1a622176e42c62d5298e13e35664aabb
vidhi <vidhi2000@utexas.edu>
2023-10-22 17:23:24 -0500

    Implemented the entirety of the reset password feature

--- src/main/urls.py
+++ src/main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("about/", views.about_us, name="about_us"),
     path("contact/", views.contact_us, name="contact_us"),
     path('contact/submit/', views.contact_us_form_submit, name='contact_us_form_submit'),
+    path("reset_password/", views.reset_password, name='reset_password')
 ]
