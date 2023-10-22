41ed13436c331a23719da474921097a857a8d3d6
Kalyan Cheerla <32354220+kalyancheerla@users.noreply.github.com>
2023-10-21 20:27:40 -0500

    Feature/django login page (#7)
    * Created base for login page
    
    * Added form frontend componenets
    
    * Fix login page to work properly with Django
    
    * Added basic login functionality
    
    * Added basic login function
    
    * login fixes
    
    ---------
    
    Co-authored-by: Emmie Abels <emmieabels1@gmail.com>

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
+    path("login/", views.user_login, name="user_login"),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def user_login(request):
+    if request.method == 'POST': # django login forms use POST method
+        form = LoginForm(request.POST) # send form to server
+        
+        if form.is_valid():
+            # clean inputs
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(home) # redirect user to home page after user is logged in
+            else:
+                form = LoginForm()
+
+    return render(request, "login.html")
 
 def signup(request):
     if request.method == 'POST':
