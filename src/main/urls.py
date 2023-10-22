87688c0a4b99bc30a3fb0873d54acfb5b2e268ed
Bhavani Rachakatla <gangaputrikarb20@gmail.com>
2023-10-21 22:29:55 -0500

    added about us and contact us pages

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about/", views.about_us, name='about_us'),
-    path("contact/", views.contact_us, name='contact_us'),
+    path("about/", views.about_us, name="about_us"),
+    path("contact/", views.contact_us, name="contact_us"),
+    path('contact/submit/', views.contact_us_form_submit, name='contact_us_form_submit'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect #HttpResponse
 
-# Create your views here.
 def home(request):
     return render(request, "index.html")
 
 def about_us(request):
-    return render(request, "about_us.html")
+    developers = [
+        {'name': 'Kalyan Cheerla', 'bio': 'Project Manager', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Bhavani Rachakatla', 'bio': 'Design and Testing Lead', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Yasmeen Haleem', 'bio': 'Requirements and Documentation Lead', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Demir Altay', 'bio': 'Implementation Lead(backend)', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Vidhi Bhatt', 'bio': 'Implementation Lead(front end)', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Emmie Abels', 'bio': 'Implementation Lead(front end)', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Manushree Buyya', 'bio': 'Demo and Presentation Lead', 'image': 'images/ProfilePic.jpg'},
+        {'name': 'Pravallika Bollavaram', 'bio': 'Configuration Management Lead', 'image': 'images/ProfilePic.jpg'},
+    ]
+    return render(request, "about_us.html", {'developers': developers})
 
 def contact_us(request):
     return render(request,"contact_us.html")
+
+def contact_us_form_submit(request):
+    if request.method == 'POST':
+        # Need to handle the form submission logic here
+        # For now, just redirecting to the same contact us page
+        return redirect('contact_us')
+    return render(request, 'contact_us.html')
