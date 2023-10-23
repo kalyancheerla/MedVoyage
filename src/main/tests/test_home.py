d338b339f4caf61c218e39063ab664b30811cb80
PravallikaBollavaram <52188835+PravallikaBollavaram@users.noreply.github.com>
2023-10-23 13:04:20 -0500

    Update test_home.py
    Added footer and landing page content test cases

--- src/main/tests/test_home.py
+++ src/main/tests/test_home.py
@@ -18,3 +18,13 @@ class HomeViewTestCases(TestCase):
         for nav_field in NavBar_Fields:
             self.assertContains(response, nav_field)
 
+    def test_footer(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "&copy; MedVoyage")
+
+    def test_content_display(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'MedVoyage')  
+        self.assertContains(response, 'Your journey to health begins here: Navigating Wellness with MedVoyage.') 
