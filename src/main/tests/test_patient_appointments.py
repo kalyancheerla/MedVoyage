925de2ae40e9d7dc80f064efea732425c0a35650
vidhi <vidhi2000@utexas.edu>
2023-11-12 19:16:55 -0600

    Unit Tests for Patient Previous/Upcoming Appt LIst

---
+++ src/main/tests/test_patient_appointments.py
@@ -0,0 +1,81 @@
+from django.test import TestCase, Client
+
+class PatientUpcomingAndPastAppointmentsTestCases(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_appt_page(self):
+        response = self.client.post('/signup/', data={
+            'username': 'pspk',
+            'first_name': 'pk',
+            'last_name': 'ps',
+            'email': 'pspk@yahoo.com',
+            'phone': '9999999999',
+            'security_question': 'Sector 401, Underworld, Mars, Milkyway',
+            'password': 'idk',
+            'password2': 'idk',
+            'login_type': 'doctor',
+        })
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post('/login/', data={
+            'username': 'pspk',
+            'password': 'idk',
+        })
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/client_appointments/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_appt_nav(self):
+        response = self.client.post('/signup/', data={
+            'username': 'pspk',
+            'first_name': 'pk',
+            'last_name': 'ps',
+            'email': 'pspk@yahoo.com',
+            'phone': '9999999999',
+            'security_question': 'Sector 401, Underworld, Mars, Milkyway',
+            'password': 'idk',
+            'password2': 'idk',
+            'login_type': 'doctor',
+        })
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post('/login/', data={
+            'username': 'pspk',
+            'password': 'idk',
+        })
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Patient Appointments")
+
+    def test_appt_no_nav_logged_out(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Patient Appointments")
+       
+    def test_no_appt_listing_when_no_appt_made(self):
+        response = self.client.post('/signup/', data={
+            'username': 'pspk',
+            'first_name': 'pk',
+            'last_name': 'ps',
+            'email': 'pspk@yahoo.com',
+            'phone': '9999999999',
+            'security_question': 'Sector 401, Underworld, Mars, Milkyway',
+            'password': 'idk',
+            'password2': 'idk',
+            'login_type': 'doctor',
+        })
+        self.assertEqual(response.status_code, 302)
+        response = self.client.post('/login/', data={
+            'username': 'pspk',
+            'password': 'idk',
+        })
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/client_appointments/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No Upcoming Appointments")
+
+    def test_page_inaccessible_when_not_logged_in(self):
+        response = self.client.get('/client_appointments/')
+        self.assertNotEqual(response.status_code, 200)
+
